38821f4df7dc0c9ed2f99cb6297f5228a9bd6740
rashed145 <115322194+rashed145@users.noreply.github.com>
2022-12-29 19:52:15 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ curl -L https://raw.githubusercontent.com/rashed145/mytools/main/colorspec -o ~/
 curl -L https://raw.githubusercontent.com/rashed145/mytools/main/.profile -o ~/.profile
 curl -L https://raw.githubusercontent.com/rashed145/mytools/main/.bashrc -o ~/.bashrc
 chmod +x ~/bin/*
-mkdir -p ~/{.termux,.fonts,.bash_themes}
+mkdir -p ~/{.termux/{.fonts,.colors},.bash_themes}
 echo -e "\n\e[1mChanging TERMUX settings...\e[0m\n"
 echo """
 allow-external-apps = true
@@ -17,10 +17,11 @@ enforce-char-based-input = true
 """|tee -a ~/.termux/termux.properties
 touch ~/.hushlogin
 echo -e "\n\e[1mDownloading Theme and Font...\e[0m\n"
-curl -L https://raw.githubusercontent.com/termux/termux-styling/master/app/src/main/assets/colors/argonaut.properties -o ~/.termux/colors.properties
+curl -L https://raw.githubusercontent.com/termux/termux-styling/master/app/src/main/assets/colors/argonaut.properties -o ~/.termux/.colors/argonaut.properties
 curl -L https://raw.githubusercontent.com/rashed145/polus-bash-theme/main/polus.bash_theme -o ~/.bash_themes/polus.bash_theme
-curl -L https://github.com/ryanoasis/nerd-fonts/blob/v2.2.2/patched-fonts/FiraCode/Medium/complete/Fira%20Code%20Medium%20Nerd%20Font%20Complete.ttf?raw=true -o ~/.fonts/FiraCode.ttf
-ln -sf ~/.fonts/FiraCode.ttf ~/.termux/font.ttf
+curl -L https://github.com/ryanoasis/nerd-fonts/blob/v2.2.2/patched-fonts/FiraCode/Medium/complete/Fira%20Code%20Medium%20Nerd%20Font%20Complete.ttf?raw=true -o ~/.termux/.fonts/FiraCode.ttf
+ln -sf ~/.termux/.fonts/FiraCode.ttf ~/.termux/font.ttf
+ln -sf ~/.termux/{.colors/argonaut.properties,colors.properties}
 termux-setup-storage
 echo -e "\n\e[1mUpdating...\e[0m\n"
 echo "deb https://packages-cf.termux.org/apt/termux-main stable main"|tee $PREFIX/etc/apt/sources.list
