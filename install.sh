5c8ad12499e3d077d235cad792a1450ef3d9b31d
rashed145 <115322194+rashed145@users.noreply.github.com>
2023-01-03 15:21:04 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ enforce-char-based-input = true
 echo "TERMUX_HUSHLOGIN=1"|tee -a ~/.termux/motd.sh
 echo -e "\n\e[1mDownloading Theme and Font...\e[0m\n"
 curl -L https://raw.githubusercontent.com/termux/termux-styling/master/app/src/main/assets/colors/argonaut.properties -o ~/.termux/.colors/argonaut.properties --create-dirs
-curl -L https://raw.githubusercontent.com/rashed145/polus-bash-theme/main/polus.bash_theme -o ~/.config/bash/themes/polus.bash-theme --create-dirs
+curl -L https://raw.githubusercontent.com/rashed145/polus-bash-theme/main/polus.bash-theme -o ~/.config/bash/themes/polus.bash-theme --create-dirs
 curl -L https://github.com/ryanoasis/nerd-fonts/blob/v2.2.2/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf?raw=true -o ~/.termux/.fonts/Hack.ttf --create-dirs
 ln -sf ~/.termux/{.fonts/Hack.ttf,font.ttf}
 ln -sf ~/.termux/{.colors/argonaut.properties,colors.properties}
