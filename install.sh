85e9c73de1d9a69cd549d582a78b7d557627199a
rashed145 <115322194+rashed145@users.noreply.github.com>
2023-01-08 12:53:24 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ curl -L https://raw.githubusercontent.com/rashed145/mytools/main/profile -o ~/.p
 curl -L https://raw.githubusercontent.com/rashed145/mytools/main/bashrc -o ~/.bashrc
 curl -L https://raw.githubusercontent.com/rashed145/mytools/main/nanorc -o ~/.config/nano/nanorc --create-dirs
 chmod +x ~/bin/*
+echo -e "\n\e[1mDownloading Theme and Font...\e[0m\n"
+curl -L https://raw.githubusercontent.com/termux/termux-styling/master/app/src/main/assets/colors/argonaut.properties -o ~/.termux/.colors/argonaut.properties --create-dirs
+curl -L https://raw.githubusercontent.com/rashed145/polus-bash-theme/main/polus.bash-theme -o ~/.config/bash/themes/polus.bash-theme --create-dirs
+curl -L https://github.com/ryanoasis/nerd-fonts/blob/v2.2.2/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf?raw=true -o ~/.termux/.fonts/Hack.ttf --create-dirs
+ln -sf ~/.termux/{.fonts/Hack.ttf,font.ttf}
+ln -sf ~/.termux/{.colors/argonaut.properties,colors.properties}
 echo -e "\n\e[1mChanging TERMUX settings...\e[0m\n"
 echo """
 allow-external-apps = true
@@ -16,12 +22,6 @@ terminal-margin-horizontal=10
 enforce-char-based-input = true
 """|tee -a ~/.termux/termux.properties
 echo "TERMUX_HUSHLOGIN=1"|tee -a ~/.termux/motd.sh
-echo -e "\n\e[1mDownloading Theme and Font...\e[0m\n"
-curl -L https://raw.githubusercontent.com/termux/termux-styling/master/app/src/main/assets/colors/argonaut.properties -o ~/.termux/.colors/argonaut.properties --create-dirs
-curl -L https://raw.githubusercontent.com/rashed145/polus-bash-theme/main/polus.bash-theme -o ~/.config/bash/themes/polus.bash-theme --create-dirs
-curl -L https://github.com/ryanoasis/nerd-fonts/blob/v2.2.2/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf?raw=true -o ~/.termux/.fonts/Hack.ttf --create-dirs
-ln -sf ~/.termux/{.fonts/Hack.ttf,font.ttf}
-ln -sf ~/.termux/{.colors/argonaut.properties,colors.properties}
 termux-setup-storage
 echo -e "\n\e[1mUpdating...\e[0m\n"
 echo "deb https://packages-cf.termux.org/apt/termux-main stable main"|tee $PREFIX/etc/apt/sources.list
