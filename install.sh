0a8ba226a00957446c8083def53125e2123a53d7
rashed145 <115322194+rashed145@users.noreply.github.com>
2023-01-16 00:35:31 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 set -e
 echo -e "\e[1mDownloading configuration files...\e[0m\n"
-curl -L https://raw.githubusercontent.com/rashed145/mytools/main/colorspec -o ~/bin/colorspec --create-dirs
-curl -L https://raw.githubusercontent.com/rashed145/mytools/main/wp -o ~/bin/wp
-curl -L https://raw.githubusercontent.com/rashed145/mytools/main/sbmb -o ~/bin/sbmb
-curl -L https://raw.githubusercontent.com/rashed145/mytools/main/profile -o ~/.profile
-curl -L https://raw.githubusercontent.com/rashed145/mytools/main/bashrc -o ~/.bashrc
-curl -L https://raw.githubusercontent.com/rashed145/mytools/main/nanorc -o ~/.config/nano/nanorc --create-dirs
+curl -fsSL https://raw.githubusercontent.com/rashed145/mytools/main/colorspec -o ~/bin/colorspec --create-dirs
+curl -fsSL https://raw.githubusercontent.com/rashed145/mytools/main/wp -o ~/bin/wp
+curl -fsSL https://raw.githubusercontent.com/rashed145/mytools/main/sbmb -o ~/bin/sbmb
+curl -fsSL https://raw.githubusercontent.com/rashed145/mytools/main/profile -o ~/.profile
+curl -fsSL https://raw.githubusercontent.com/rashed145/mytools/main/bashrc -o ~/.bashrc
+curl -fsSL https://raw.githubusercontent.com/rashed145/mytools/main/nanorc -o ~/.config/nano/nanorc --create-dirs
 chmod +x ~/bin/*
 echo -e "\n\e[1mDownloading Theme and Font...\e[0m\n"
-curl -L https://raw.githubusercontent.com/termux/termux-styling/master/app/src/main/assets/colors/argonaut.properties -o ~/.termux/.colors/argonaut.properties --create-dirs
-curl -L https://raw.githubusercontent.com/rashed145/polus-bash-theme/main/polus.bash-theme -o ~/.config/bash/themes/polus.bash-theme --create-dirs
+curl -fsSL https://raw.githubusercontent.com/termux/termux-styling/master/app/src/main/assets/colors/argonaut.properties -o ~/.termux/.colors/argonaut.properties --create-dirs
+curl -fsSL https://raw.githubusercontent.com/rashed145/polus-bash-theme/main/polus.bash-theme -o ~/.config/bash/themes/polus.bash-theme --create-dirs
 curl -L https://github.com/ryanoasis/nerd-fonts/blob/v2.2.2/patched-fonts/FiraCode/Medium/complete/Fira%20Code%20Medium%20Nerd%20Font%20Complete.ttf?raw=true -o ~/.termux/.fonts/FiraCode.ttf --create-dirs
 ln -sf ~/.termux/{.fonts/FiraCode.ttf,font.ttf}
 ln -sf ~/.termux/{.colors/argonaut.properties,colors.properties}
@@ -27,6 +27,6 @@ echo "TERMUX_HUSHLOGIN=1"|tee -a ~/.termux/motd.sh
 termux-setup-storage
 echo -e "\n\e[1mUpdating...\e[0m\n"
 echo "deb https://packages-cf.termux.org/apt/termux-main stable main"|tee $PREFIX/etc/apt/sources.list
-apt update && yes|apt upgrade
+apt update && yes|apt full-upgrade
 echo "deb https://grimler.se/termux/termux-main stable main"|tee $PREFIX/etc/apt/sources.list
 apt update && yes|apt install man tree
