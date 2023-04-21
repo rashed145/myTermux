571681064de08196a56aeff1c7c5d50fa920517c
rashed145 <115322194+rashed145@users.noreply.github.com>
2023-04-21 12:30:51 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ echo "deb https://packages-cf.termux.org/apt/termux-main stable main"|tee $PREFI
 apt update && yes|apt dist-upgrade
 echo "deb https://grimler.se/termux/termux-main stable main"|tee $PREFIX/etc/apt/sources.list
 }||true
-apt update && yes|apt install man tree
+apt update && yes|apt install man tree python clang- python-ensurepip-wheels- aria2
 echo -e "\n\e[1mDownloading FFMPEG\e[0m"
 curl -L https://github.com/Khang-NT/ffmpeg-binary-android/releases/download/2018-07-31/arm64-v8a-lite.tar.bz2 -o ~/.archives/ffmpeg.tar.bz2 --create-dirs
 tar xf ~/.archives/ffmpeg.tar.bz2 -C ~/bin
