101e055ed579c815c7cee9fa3a8a44f358b4d348
rashed145 <115322194+rashed145@users.noreply.github.com>
2023-06-27 00:57:23 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ terminal-transcript-rows = 3000
 terminal-cursor-style = bar
 bell-character = ignore
 terminal-margin-horizontal=10
-extra-keys=[['ALT','TAB','CTRL','$','-','_','LEFT','DOWN','UP','RIGHT']]
+extra-keys=[['ALT','TAB','CTRL','$','-','"','LEFT','DOWN','UP','RIGHT']]
 """|tee ~/.termux/termux.properties
 echo "TERMUX_HUSHLOGIN=1"|tee -a ~/.termux/motd.sh
 test -w /sdcard||termux-setup-storage
