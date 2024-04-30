7eff48f69ff8a5c2f59436fbe7c0b48056e1eae0
Rashedul Hasan Rafi <rashedrafi145@gmail.com>
2024-04-30 16:33:26 +0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,26 +1,19 @@
 set -e
 dir=~
-repo="https://raw.githubusercontent.com/rashed145/myTermux/master"
+apt-get -q=1 update && yes|apt-get -q=1 dist-upgrade man pick && apt-get -q=1 update
+test -w /sdcard||termux-setup-storage
+>$dir/.hushlogin
+d="https://raw.githubusercontent.com"
+repo="$d/rashed145/myTermux/master"
 font="$repo/CaskaydiaCove.ttf"
-color="https://raw.githubusercontent.com/termux/termux-styling/master/app/src/main/assets/colors/argonaut.properties"
-theme="https://raw.githubusercontent.com/rashed145/polus-bash-theme/master/polus.bash-theme"
+color="$d/termux/termux-styling/master/app/src/main/assets/colors/argonaut.properties"
+theme="$d/rashed145/polus-bash-theme/master/polus.bash-theme"
 curl --parallel-immediate -C - -ZfsSL "$repo/{.bashrc,.profile,.nanorc,.aliases}" -o $dir/"#1" $color -o $dir/.termux/colors.properties $theme -o $dir/.config/bash/themes/theme.sh $font -o $dir/.termux/font.ttf --create-dirs
-printf """\
+echo """
 allow-external-apps = true
 disable-terminal-session-change-toast = true
-terminal-transcript-rows = 4000
+terminal-transcript-rows = 3000
 terminal-cursor-style = bar
 bell-character = ignore
 terminal-margin-horizontal=10
-extra-keys=[['ESC','TAB','CTRL','ALT','-','LEFT','DOWN','UP','RIGHT']]
-\n""" >> $dir/.termux/termux.properties
-printf "TERMUX_HUSHLOGIN=1\n" >> $dir/.termux/motd.sh
-test -w /sdcard||termux-setup-storage
-printf "\e[1;92mDONE:\e[mConfiguration\n\n"
-inia() { printf "deb https://packages-cf.termux.dev/apt/termux-main/ stable main\n" > $PREFIX/etc/apt/sources.list;apt-get -q=2 update; }
-inia && yes|apt-get -q=2 dist-upgrade
-inia && yes|apt-get -q=2 install man pick
-printf "\e[1;92mDONE:\e[mApt setup\n\n"
-curl --parallel-immediate -ZL $repo/arm64-v8a-lite.tar.bz2 -O --output-dir $dir/bin --create-dirs
-tar xf $dir/bin/arm64-v8a-lite.tar.bz2 -C $dir/bin && rm $dir/bin/arm64-v8a-lite.tar.bz2 && chmod +x $dir/bin/*
-printf "\e[1;93mADDED:\e[mFFMpeg\n"
+""" >> $dir/.termux/termux.properties
